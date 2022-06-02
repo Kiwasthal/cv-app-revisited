d0cd5d0c5f301b7efcd90e1c36775866cbb73ccb
Steerpike <imthekoul@gmail.com>
2022-06-03 02:43:14 +0300

    App change theme useEffect hook added

--- src/App.js
+++ src/App.js
@@ -6,64 +6,49 @@ import Title from './Components/Title';
 
 function App() {
   const [theme, setTheme] = useState({
-    main: 'purple',
-    secondary: 'red',
+    main: '#94a3b8',
+    secondary: '#4b5563',
   });
 
-  const changeTheme = theme => {
-    let index;
-    const themeLibrary = [
-      { main: 'red', secondary: 'blue' },
-      { main: 'purple', secondary: 'green' },
-      { main: 'pink', secondary: 'black' },
-      { main: 'orange', secondary: 'yellow' },
-    ];
-    themeLibrary.map((obj, arrayIndex) => {
-      if (obj.main === theme.main) {
-        index = arrayIndex;
-      }
-    });
-    if (index === themeLibrary.length - 1) index = -1;
-    setTheme({
-      main: themeLibrary[index + 1].main,
-      secondary: themeLibrary[index + 1].secondary,
-    });
-  };
-
-  const useKeyPress = targetKey => {
-    useEffect(() => {
-      window.addEventListener('keydown', e => {
-        changeTheme(theme);
+  useEffect(() => {
+    const changeTheme = e => {
+      if (e.keyCode !== 32) return;
+      let index;
+      const themeLibrary = [
+        { main: '#94a3b8', secondary: '#4b5563' },
+        { main: '#737373', secondary: '#44403c' },
+        { main: '#f87171', secondary: '#7f1d1d' },
+        { main: '#fb923c', secondary: '#9a3412' },
+        { main: '#fbbf24', secondary: '#a16207' },
+        { main: '#84cc16', secondary: '#365314' },
+        { main: '#2dd4bf', secondary: '#155e75' },
+        { main: '#60a5fa', secondary: '#075985' },
+        { main: '#a855f7', secondary: '#581c87' },
+        { main: '#e879f9', secondary: '#701a75' },
+        { main: '#fda4af', secondary: '#9f1239' },
+      ];
+      themeLibrary.map((obj, arrayIndex) =>
+        obj.main === theme.main ? (index = arrayIndex) : obj
+      );
+      if (index === themeLibrary.length - 1) index = -1;
+      setTheme({
+        main: themeLibrary[index + 1].main,
+        secondary: themeLibrary[index + 1].secondary,
       });
-      // return () => {
-      //   window.removeEventListener('keydown', e => {
-      //     changeTheme(theme);
-      //   });
-      // };
-    }, []);
-  };
-  const space = useKeyPress('space');
+    };
+    window.addEventListener('keydown', changeTheme);
+    return () => window.removeEventListener('keydown', changeTheme);
+  }, [theme]);
 
   return (
     <div className="App">
-      {space}
-      <button
-        onClick={e => {
-          changeTheme(theme);
-        }}
-      ></button>
       <ThemeProvider theme={theme}>
         <Title />
-        <Button>HEy</Button>
         <RightPartition />
       </ThemeProvider>
     </div>
   );
 }
-
-const Button = styled.button`
-  background-color: ${props => props.theme.main};
-`;
 
 const RightPartition = styled.div`
   grid-area: 1 / 2 / 4 / 3;
