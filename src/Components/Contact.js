65f351f84a7f64ff953456d333bb7133f9e237c0
Steerpike <imthekoul@gmail.com>
2022-06-03 12:12:17 +0300

    Created SVG's - grid placement

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,51 @@
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLocationDot,
+  faGlobe,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
   return (
     <StyledContactContainer>
       <StyledContactHeader>CONTACT</StyledContactHeader>
-      <StyledSectionsWrapper />
+      <StyledSectionsWrapper>
+        <StyledContactSection>
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            style={{
+              gridArea: 'icon',
+              justifySelf: 'center',
+              alignSelf: 'center',
+              height: '40px',
+            }}
+          />
+        </StyledContactSection>
+        <StyledContactSection>
+          <StyledSectionHeader>Adress</StyledSectionHeader>
+          <FontAwesomeIcon
+            icon={faGlobe}
+            style={{
+              gridArea: 'icon',
+              justifySelf: 'center',
+              alignSelf: 'center',
+              height: '40px',
+            }}
+          />
+        </StyledContactSection>
+        <StyledContactSection>
+          <FontAwesomeIcon
+            icon={faPhone}
+            style={{
+              gridArea: 'icon',
+              justifySelf: 'center',
+              alignSelf: 'center',
+              height: '35px',
+            }}
+          />
+        </StyledContactSection>
+      </StyledSectionsWrapper>
     </StyledContactContainer>
   );
 };
@@ -18,7 +59,7 @@ const StyledContactContainer = styled.div`
 `;
 
 const StyledContactHeader = styled.h2`
-  color: ${props => props.theme.main};
+  color: #fff;
   justify-self: center;
   align-self: center;
   padding: 5px 3px;
@@ -29,6 +70,24 @@ const StyledContactHeader = styled.h2`
 const StyledSectionsWrapper = styled.div`
   display: grid;
   grid-template-rows: repeat(3, 1fr);
+`;
+
+const StyledContactSection = styled.div`
+  display: grid;
+  grid-template-columns: 30% 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-areas:
+    'icon header'
+    'icon informationFirst'
+    'icon  informationSecond';
+`;
+
+const StyledSectionHeader = styled.h5`
+  justify-self: flex-start;
+  align-self: center;
+  margin: 0;
+  color: #fff;
+  grid-area: header;
 `;
 
 export default Contact;
